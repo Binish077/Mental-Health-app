c169857f35b20456c8a4fe55891216ee52946226
Binish Patel <76157635+Binish077@users.noreply.github.com>
2021-05-21 21:28:38 +0530

    Create main-app.py

---
+++ main-app.py
@@ -0,0 +1,11 @@
+import numpy as np
+from tkinkter import *
+import time
+
+def mindful():
+  set_time=float((input("How long (minutes) would you like today's meditation to be? (Default: 5 minutes) "))*60)
+  if set_time=="":
+    set_time=300
+  
+  
+  
