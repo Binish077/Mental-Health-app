a9c31e10e877c9de37731f7deed5b881f345bdb1
Binish Patel <76157635+Binish077@users.noreply.github.com>
2021-05-22 01:35:28 +0530

    just removing some extra space

--- quiz.py
+++ quiz.py
@@ -1,7 +1,7 @@
 #this code asks the user for symptoms and gives a diagnosis
 def stress():
     print("Are you experiencing aches and pains, headaches, and/or exhaustion? (Y/N)\n")
-    s1=input("\n")
+    s1=input("")
     if s1=="Y":
         print("You may be experiencing stress")
         print("Symptoms also may include: Racing heart, weak immune systems, muscle tension, etc.")
@@ -10,7 +10,7 @@ def stress():
 
 def depression():
     print("Are you experiencing anger, emptiness, loss of interest and/or reduced concentration? (Y/N)\n")
-    d1=input("\n")
+    d1=input("")
     if d1=="Y":
         print("You may be experiencing depression")
         print("Symptoms also may include: irritability, anxiousness, loss of appetite, feeling tired easily, etc.")
@@ -19,7 +19,7 @@ def depression():
 
 def anxiety():
     print("Are you experiencing difficulty concentrating, fatigue and exhaustion, sweaty palms, and/or shaking? (Y/N)\n")
-    a1=input("\n")
+    a1=input("")
     if a1=="Y":
         print("You may be experiencing Generalized Anxiety Disorder")
         print("Symptoms also may include: irritability, muscle tension, difficulty sleeping, etc.")
