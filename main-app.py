5561da6825c4b4c2d932bb415a64e94d62670d9c
Binish077 <bsh.pat77@gmail.com>
2021-05-21 22:26:45 +0530

    completed function mindful
    this could be better, but we have the first edition now

--- main-app.py
+++ main-app.py
@@ -1,18 +1,26 @@
 import numpy as np
-from tkinkter import *
+from tkinter import *
 import time
+import random
 
 def mindful():
-  set_time=float((input("How long (minutes) would you like today's meditation to be? (Default: 5 minutes) "))*60)
-  if set_time=="":
+  minutes=input("How long (minutes) would you like today's meditation to be? (Default: 5 minutes) ")
+  if minutes=="":
     set_time=300
+  else:
+      set_time=float(minutes)*60
+
   print("Timer starts now")
-  for i in range(set_time):
+  for i in range(int(set_time)):
     time.sleep(1)
     set_time=set_time-1
     if set_time%5==0:
-      print("Inhale/Exhale") #I'm thinking of the 4-7-8 breathing technique but I'm not sure how to implement it
-    
-  
-  
-  
+      print("Inhale/Exhale\n") #I'm thinking of the 4-7-8 breathing technique but I'm not sure how to implement it
+    if set_time%45==0:
+        #a few relaxing statements to be printed every minutes
+        s1="Let go of any worries and thoughts you might have"
+        s2="Focus on your breathing"
+        s3="You're doing well"
+        s4="Relax your muscules"
+        quotes=[s1,s2,s3,s4]
+        print(random.choice(quotes)+"\n")
